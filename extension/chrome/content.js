3b792e8b3646ce83ff6cf764f4058eda9cd8c2ce
kodango <dangoakachan@foxmail.com>
2014-04-25 23:06:45 +0800

    work again...

--- extension/chrome/content.js
+++ extension/chrome/content.js
@@ -1,5 +1,5 @@
 /*
- * Content Script Js
+ * Content Script
  *
  * Description:
  * 
@@ -22,82 +22,83 @@
     "use strict";
 
     /*
-     * Shorthand functions
+     * Override the rwt function
      */
-    function dec(s) { return window.decodeURIComponent(s); }
+    function overrideRwt() {
+        var ele = document.createElement('script');
 
-    /* 
-     * Removes click-tracking and restores redirection URL to original one.
+        ele.type = 'text/javascript';
+
+        /* Inject the function into document */
+        function injectFunc() {
+            Object.defineProperty(window, 'rwt', {
+                value: function() { return true; },
+                writable: false,
+                configurable: false
+            });
+        }
+
+        ele.textContent = '(' + injectFunc + ')()';
+        document.body.appendChild(ele);
+    }
+
+    /*
+     * Clean the link and prevent the url to be rewritten
      */
-    function cleanLink(link) {
-        /* Click-tracking is bind on mousedown event. */
-        var track_code = link.getAttribute('onmousedown');
-        /* Redirection URL's regular expression */
-        var rredirect = /\/url\?(?:url|q)=([^&]*)/i;
-        /* Real image URL's regular expression */
-        var rrealimg = /imgurl=([^&]*)/i;
+    function cleanTheLink(event) {
+        var a = event.target, depth = 1;
 
-        if (track_code && track_code.indexOf('return rwt(') != -1) {
-            link.removeAttribute('onmousedown');
-        } else if (rredirect.test(link.href)) {
-            link.href = dec(link.href.match(rredirect)[1]);
-        } else if (rrealimg.test(link.href)) {
-            link.href = dec(link.href.match(rrealimg)[1]);
+        /* Found the target link, and try to clean it */
+        while (a && a.tagName != 'A' && depth-- > 0)
+            a = a.parentNode;
+
+        if (!a || a.tagName != 'A')
+           return;
+
+        if (a.dataset['cleaned'] == 1) // Return if a link has been already cleaned
+            return;
+
+        var need_clean = false;
+
+        /* Find the original url */
+        var result = /\/(?:url|imgres).*[&?](?:url|q|imgurl)=([^&]+)/i.exec(a.href);
+
+        /* Restores the redirection url */
+        if (result) {
+            a.href = decodeURIComponent(result[1]);
+            need_clean = true;
+        }
+
+        var val = a.getAttribute('onmousedown') || '';
+
+        /* Remove the onmousedown attribute */
+        if (val.indexOf('return rwt(') != -1) {
+            a.removeAttribute('onmousedown');
+            need_clean = true;
+        }
+                        
+        var cls = a.className || '';
+
+        if (cls.indexOf('irc_') != -1)
+            need_clean = true;
+
+        /* Remove all event listener from the element */
+        if (need_clean) { 
+            var clone = a.cloneNode(true);
+
+            a.parentNode.replaceChild(clone, a);
+            clone.dataset['cleaned'] = 1;
         }
     }
 
     /*
-     * Removes click-tracking and restores redirection in all search results.
+     * The main entry
      */
-    function removeRedirect(event) {
-        /* Queries all result links */
-        var links = document.querySelectorAll('#res a, #rhs a');
-        var len = links.length;
-
-        /* Iterates each link that found in the result and clean it */
-        for (var i = 0; i < len; i++) {
-            cleanLink(links[i]);
-        }
+    function main()
+    {
+        overrideRwt();
+        document.addEventListener('mouseover', cleanTheLink, true);
     }
 
-    /*
-     * Removes click-tracking and restores redirection in all search results
-     * until the Instant Search is complete, that means all results are
-     * displayed in the search result page.
-     */
-    function delayRemoveRedirect(event) {
-        /*
-         * Web page is loading from top to bottom, so we choose an element 
-         * that is located after the result lists and will change in one
-         * search. The foot (div#xfoot) element meets both requirements. 
-         *
-         * DOM Mutation Events can detect whether a dom element is changed.
-         */
-        if (event.relatedNode.id == 'xfoot') {
-            removeRedirect(event);
-            event.stopPropagation();
-        }
-    }
-
-    /* 
-     * Listens specified events and handle it 
-     */
-    function listenEvents() {
-        /* 
-         * Listens DOMContentLoaded event in normal search, this event will
-         * not fire in Instant Search.
-         */
-        if (location.pathname == '/search') {
-            window.addEventListener("DOMContentLoaded", removeRedirect, false);
-        }
-
-        /*
-         * Listen the specified mutation event in document object, but we
-         * use event delegate here. This will save a lot of unnecessary
-         * performance overhead.
-         */
-        document.addEventListener('DOMNodeInserted', delayRemoveRedirect, false);
-    }
-
-    listenEvents();
+    main();
 })(window);
